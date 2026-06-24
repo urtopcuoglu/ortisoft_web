1a5967e2b7624771a01bf0393a40346b2b55d9c6
utopcuoglu <umutcanreceptopcuoglu@aof.anadolu.edu.tr>
2026-06-24 21:54:45 +0300

    feat: next-sitemap ve robots.txt eklendi

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ortisoft.com.tr',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
